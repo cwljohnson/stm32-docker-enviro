5ad46e07ddb2f568454d6dfef7523cb0efe19e31
Calvin <calvinofthejs@gmail.com>
2024-04-06 20:55:59 +1000

    add STM32G4 platform

--- build-stm32.sh
+++ build-stm32.sh
@@ -3,3 +3,4 @@
 docker build stm32-dev/ -f ./stm32-dev/Base.Dockerfile -t cwljohnson/stm32-dev-base
 docker build stm32-dev/ -f ./stm32-dev/STM32F0.Dockerfile -t cwljohnson/stm32-dev-f0
 docker build stm32-dev/ -f ./stm32-dev/STM32F2.Dockerfile -t cwljohnson/stm32-dev-f2
+docker build stm32-dev/ -f ./stm32-dev/STM32G4.Dockerfile -t cwljohnson/stm32-dev-g4
--- docker-push.sh
+++ docker-push.sh
@@ -3,3 +3,4 @@
 docker push cwljohnson/stm32-dev-base
 docker push cwljohnson/stm32-dev-f0
 docker push cwljohnson/stm32-dev-f2
+docker push cwljohnson/stm32-dev-g4
